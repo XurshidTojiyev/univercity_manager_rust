f6c351dfbc57f305d8fab4963b9778a2c16645a7
plooton <tojiyev82@list.ru>
2024-05-27 00:55:39 -0700

    first commit teacher is created

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod mods;
+use mods::univer_manager::pupil;
+use crate::mods::univer_manager::Teacher;
+
+fn main() {
+    let mut p1: Teacher = Teacher::new("Matuna".to_string(), "Karimova".to_string(), 19, "Math".to_string());
+    p1.set_name("Halima".to_string());
+
+    println!("{:?}", p1);
+}
---
+++ src/mods.rs
@@ -0,0 +1,51 @@
+pub mod univer_manager {
+    pub struct pupil {
+        pub name: String,
+        pub surname: String,
+        pub age: i32,
+        pub class: i32,
+        pub class_letter: char,
+        pub teacher: Teacher
+    }
+
+    #[derive(Debug)]
+    pub struct Teacher {
+        pub name: String,
+        pub surname: String,
+        pub age: i32,
+        pub subject: String
+    }
+
+    impl Teacher {
+        pub fn new(name1: String,surname1: String, age1: i32, subject1: String) -> Teacher {
+            Teacher{name: name1, surname: surname1, age: age1, subject: subject1}
+        }
+
+        pub fn get_name(&self) -> String {
+            self.name.to_string()
+        }
+
+        pub fn get_surname(&self) -> String {
+            self.surname.to_string()
+        }
+
+        pub fn get_age(&self) -> i32{
+            self.age
+        }
+
+        pub fn get_subject(&self) -> String{
+            self.subject.to_string()
+        }
+
+        pub fn set_surname(&mut self, surname: String){
+            self.surname = surname.to_string()
+        }
+        pub fn set_age(&self, age: i32){}
+        pub fn set_name(&mut self, name: String){
+            self.name = name.to_string()
+        }
+        pub fn set_subject(&mut self, subject: String){
+            self.subject = subject.to_string()
+        }
+    }
+}
